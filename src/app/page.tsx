87f583b89ca0a143142e62e542715d4ff890680c
Zylan <zihan.jian@outlook.com>
2025-04-25 10:57:48 +0800

    新增72色调色板，更新调色板选项以包含新调色板，同时保留T1颜色，优化代码结构以支持更多调色板。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,9 +47,12 @@ const palette96Keys = ["A3", "A4", "A6", "A7", "A10", "A11", "A13", "A14", "B3",
 // 120 Color Palette Keys (from user table)
 const palette120Keys = ["A1", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "A12", "A13", "A14", "A15", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B10", "B11", "B12", "B13", "B14", "B15", "B16", "B17", "B18", "B19", "B20", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C13", "C14", "C15", "C16", "C17", "D1", "D2", "D3", "D5", "D6", "D7", "D8", "D9", "D11", "D12", "D13", "D14", "D15", "D16", "D17", "D18", "D19", "D20", "D21", "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9", "E10", "E11", "E12", "E13", "E14", "E15", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13", "F14", "G1", "G2", "G3", "G5", "G6", "G7", "G8", "G9", "G13", "G14", "G17", "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H12", "M5", "M6", "M9", "M12", "T1"]; // Added T1
 
+// 72 Color Palette Keys (from user table)
+const palette72Keys = ["A3", "A4", "A6", "A7", "A10", "A11", "A13", "B3", "B5", "B7", "B8", "B10", "B12", "B14", "B17", "B18", "B19", "B20", "C2", "C3", "C5", "C6", "C7", "C8", "C10", "C11", "C13", "C16", "D2", "D3", "D6", "D7", "D8", "D9", "D11", "D12", "D13", "D14", "D15", "D16", "D18", "D19", "D20", "D21", "E1", "E2", "E3", "E4", "E5", "E8", "E12", "E13", "F5", "F7", "F8", "F10", "F13", "G1", "G2", "G3", "G5", "G7", "G8", "G9", "G13", "H1", "H2", "H3", "H4", "H5", "H7", "T1"]; // Added T1
+
 // Placeholder for other palettes
-// const palette72Keys = [...];
-// etc.
+// const palette48Keys = [...];
+// const palette24Keys = [...];
 
 const paletteOptions = {
   'all': { name: `221全实色`, keys: allPaletteKeys },
@@ -57,6 +60,7 @@ const paletteOptions = {
   '144': { name: '144色', keys: palette144Keys },
   '120': { name: '120色', keys: palette120Keys },
   '96': { name: '96色', keys: palette96Keys },
+  '72': { name: '72色', keys: palette72Keys }, // Added 72
   // Add other palettes here
 };
 
